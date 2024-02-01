98d44bf247239dfb854d23734560ad4a065b5998
belausabaev <73948803+belausabaev@users.noreply.github.com>
2024-02-01 19:53:56 +0100

    Add files via upload

--- js/gui-test.js
+++ js/gui-test.js
@@ -1,25 +1,33 @@
-const PARAMS = {
+
+
+
+
+  const PARAMS = {
     sound: 0, //sample file number in GUI drop down list
-    map: 3,
-    attack: 0.3, //in seconds
-    decay: 0.3, //in seconds
-    density: 50,
-    delay: 0.1,
-    feedback: 0.1,
-    spread: 0,
-    pitch: 1,
-    areas: 3
+  map: 3,
+  attack: 0.3, //in seconds
+  decay: 0.3, //in seconds
+  density: 50,
+  delay: 0.1,
+  feedback: 0.1,
+  spread: 0,
+  pitch: 1,
+  areas: 3,
+  description : "bird voices are fading out and are replaced by insects"
   };
-  
-  
-  
-  const pane = new Tweakpane({
+
+
+  const pane = new Tweakpane.Pane({
     title: 'ClimaSynth',
-    expanded: true,
-  });
-  
+    expanded: true
+  }
+
+  );
+
+
+
   pane.addSeparator();
-  
+
   /*
   const snd = pane.addFolder({
     title: 'Sound',
@@ -27,70 +35,76 @@ const PARAMS = {
   */
 
   const SourceInput =
-    pane.addInput(PARAMS, 'sound', { options: { "birds near water": 0, "river water": 1, "treebark": 2 } });
+  pane.addInput(PARAMS, 'sound', {options: {"birds near water": 0, "river water": 1, "treebark": 2 } });
   SourceInput.on('change', function (ev) {
     console.log(ev.value);
-    if (ev.value == 0) {
-      bufferSwitch(0);
-  
+  if (ev.value == 0) {
+    PARAMS.description = "bird voices are fading out and are replaced by insects";
+    bufferSwitch(0);
     }
-    if (ev.value == 1) {
-      bufferSwitch(1);
+  if (ev.value == 1) {
+    PARAMS.description = "river flows are shrinking and moving differently as they are drying out";
+    bufferSwitch(1);
     }
-    if (ev.value == 2) {
-      bufferSwitch(2);
+  if (ev.value == 2) {
+    PARAMS.description = "tree barks are becoming warmer, drier and less flexible";
+    bufferSwitch(2);
     }
   });
-  
+
+
+  pane.addMonitor(PARAMS, 'description', {
+    multiline: true,
+    lineCount: 1,
+  });
+
   /*
   const instr = pane.addFolder({
     title: 'Canvas',
   });
-*/
+  */
   /*
-  
-  const mapInput = pane.addInput(PARAMS, 'map', { options: { map3: 2 } });
+
+  const mapInput = pane.addInput(PARAMS, 'map', {options: {map3: 2 } });
   mapInput.on('change', function (ev) {
     console.log(ev.value);
-    if (ev.value == 0) {
-      loadmap(0);
+  if (ev.value == 0) {
+    loadmap(0);
     }
-    if (ev.value == 1) {
-      loadmap(1);
+  if (ev.value == 1) {
+    loadmap(1);
     }
-    if (ev.value == 2) {
-      loadmap(2);
+  if (ev.value == 2) {
+    loadmap(2);
     }
   });
   */
-  pane.addSeparator();
 
-  const cInput = pane.addInput(PARAMS, 'areas', { min: 1, max: 7, step: 1 });
+  const cInput = pane.addInput(PARAMS, 'areas', {min: 1, max: 7, step: 1 });
   cInput.on('change', function (ev) {
     areas = parseInt(ev.value);
   });
-  
+
 
   /*
   const area = pane.addFolder({
     title: 'Grain Params',
-    expanded: true
+  expanded: true
   });
   */
-  
-  pane.addSeparator();
 
-  const sprInput = pane.addInput(PARAMS, 'spread', { min: 0, max: 10, step: 1 });
+
+  const sprInput = pane.addInput(PARAMS, 'spread', {min: 0, max: 10, step: 1 });
   sprInput.on('change', function (ev) {
     spread = parseInt(ev.value);
   });
   /*
   const effects = pane.addFolder({
     title: 'Effekt Params',
-    expanded: true
+  expanded: true
   });
-  
-*/
+
+  */
 
   
   toggleBtnColorActive = (btnName) => {
@@ -100,3 +114,6 @@ const PARAMS = {
   toggleBtnColorDeact = (btnName) => {
     btnName.style.opacity = "0.7";
   };
+
+
+
