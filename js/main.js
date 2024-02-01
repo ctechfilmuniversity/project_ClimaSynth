0f983304403769c9d5c52d860c97b6ff9d23d7a8
belausabaev <73948803+belausabaev@users.noreply.github.com>
2024-02-01 09:47:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@ var spread = parseInt(PARAMS.spread);
 var del = parseFloat(PARAMS.delay.toFixed(1));
 var fb = parseFloat(PARAMS.feedback.toFixed(1));
 var density = parseInt(PARAMS.density);
-var pitch = parseFloat(PARAMS.pitchval.toFixed(1));
+var pitch = parseFloat(PARAMS.pitch.toFixed(1));
 
 
 var ctx, master, cVerb, irBuff
